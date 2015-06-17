91009b0831bdb88f7fc5c99b4661be7931b293ef
Tobias Gerschner <tobias.gerschner@rataworks.com>
2015-06-17 20:33:34 +1200

    test commit for hipchat webhook

--- lib/tiny_dyno/persistable.rb
+++ lib/tiny_dyno/persistable.rb
@@ -1,6 +1,37 @@
 module TinyDyno
   module Persistable
     extend ActiveSupport::Concern
+    #
+    # module ClassMethods
+    #
+    #   # Prepare a request to be sent to the aws-sdk
+    #   # in compliance with
+    #   # http://docs.aws.amazon.com/sdkforruby/api/Aws/DynamoDB/Client.html#put_item-instance_method
+    #
+    #   def create(attributes = nil, &block)
+    #     doc = new(attributes, &block)
+    #     doc.save
+    #   end
+    #
+    #   def save(options = {})
+    #     if new_record?
+    #       request_put_item(options).new_record?
+    #     else
+    #       request_update_item(options)
+    #     end
+    #   end
+    #
+    #   private
+    #
+    #   def request_put_item(options)
+    #     binding.pry
+    #   end
+    #
+    #   def request_update_item(options)
+    #
+    #   end
+    #
+    # end
 
   end
 end
